1b746a31508f0186f31c64046d403d3a3bcacbe3
Simone Conti <simonecnt@gmail.com>
2023-01-20 11:35:27 -0500

    Add RRUFF mineral database from 2022-06-05

---
+++ src/spectropy/reference_library/unzip.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+d=220605
+
+unzip -u -d excellent_unoriented/ excellent_unoriented_${d}.zip
+unzip -u -d fair_unoriented/ fair_unoriented_${d}.zip
+unzip -u -d poor_unoriented/ poor_unoriented_${d}.zip
+unzip -u -d unrated_unoriented/ unrated_unoriented_${d}.zip
+
+
