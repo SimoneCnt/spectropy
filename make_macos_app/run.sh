f42c37cddc7e3e44e000cc986ed85792ff365354
Simone Conti <simonecnt@gmail.com>
2025-04-05 09:50:02 -0700

    Fix brew path in run.sh; v2.0.1

--- make_macos_app/run.sh
+++ make_macos_app/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 echo "Welcome to spectropy!"
 echo "If there is any error it will be printed below..."
 
-python3 -m spectropy
+spectropy
 
